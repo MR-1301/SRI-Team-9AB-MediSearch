91ef85157f9c2331b50e6bbd48e2bcae6a342855
MR-1301 <mahir2cool@gmail.com>
2021-06-29 15:30:10 +0530

    "Added all Shops Page For users"

---
+++ public/javascript/allShops.js
@@ -0,0 +1,24 @@
+document.getElementById("myInput").addEventListener("keyup",function()
+{
+    let filter=document.getElementById("myInput").value.toUpperCase();
+    let myTable=document.getElementById("myTable");
+    let tr=myTable.getElementsByTagName('tr');
+    console.log(tr.length);
+    for(var i=0;i<tr.length;i++)
+    {
+     
+        let td=tr[i].getElementsByTagName('td')[0];
+        if(td)
+        {
+            let textvalue=td.textContent || td.innerHTML;
+            if(textvalue.toUpperCase().indexOf(filter)>-1)
+            {
+                tr[i].style.display="";
+            }             
+            else
+            {
+                tr[i].style.display="none";
+            }
+        }
+    }
+});
